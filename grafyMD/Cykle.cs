94b1fc740c6b16f02232dc022de2dbb72b02417b
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2022-04-04 20:52:51 +0200

    Działa

---
+++ grafyMD/Cykle.cs
@@ -0,0 +1,47 @@
+﻿
+// Graf, okreslanie rozmiaru
+
+// n = liczba wierzchołków grafu, graf ma byc symetryczna tablica
+
+//Graf wypelnianie macierzy zerami
+
+//Macierz wierzchołków
+//Macierz wag
+
+
+//Podawane prawdopodobienstwa istnienia krawedzi
+
+
+
+//szukanie cykli o dlugosci 3
+
+
+// TODO WYKMINIC TĄ MACIERZ C W SUMIE + POPRAWIC WAGI
+
+using System.Linq;
+
+internal class Cykle
+{
+    public List<int[]> cykl;
+    public Cykle()
+    {
+        cykl = new List<int[]>();
+    }
+    public bool czyJest(int i, int j, int k)
+    {
+        foreach (int[] n in cykl)
+        {
+            if (n.Contains(i))
+            {
+                if (n.Contains(j))
+                {
+                    if (n.Contains(k))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
--- grafyMD/Program.cs
+++ grafyMD/Program.cs
@@ -28,69 +28,81 @@ Console.WriteLine("Zalecane to 0.2, 0.25, 0.3");
 double p = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Wybrałeś prawodopodobieństwo: " +p +"\n");
 
-Random rand = new Random(2); 
-
-for (int i = 1; i < n-1; i++)
+Random rand = new Random(); 
+while(true)
 {
-    for (int j = i+1; j < n; j++)
+    for (int i = 0; i < n - 1; i++)
     {
-        double a = rand.NextDouble();
-        
-        //Tworzenie krawedzi oraz nadanie jej wagi jesli losowa liczba jest mniejsza niz prawdopodobieństwo podane przez użytkownika
-        if(a < p)
+        for (int j = i + 1; j < n; j++)
         {
-            double losowaLiczba = rand.NextDouble();
-            
-            //symetrycznośc macierzy 1 = istnieje krawędź
-            A[i, j] = 1;
-            A[j, i] = 1;
-           // Console.WriteLine("Wierzchołek w: ("+i+","+j+")");
-            
-            //Określanie wagi 
-            // w programie podano na czerwono 50 ale 50 daje max wartosc dla 10+50*(1) = 60 a w zalozenaich programu jest W >11 i W <110 wiec mnoznik chyba musi byc 100 albo jestem glupi???
-            int mnoznik = 100;
-            W[i,j] = Math.Floor(10 + mnoznik*(losowaLiczba));
-            W[j, i] = W[i, j];
+            double a = rand.NextDouble();
 
-            //Test czy to dziala
-            //Console.WriteLine(W[i, j] +" - " +W[j, i]);
+            //Tworzenie krawedzi oraz nadanie jej wagi jesli losowa liczba jest mniejsza niz prawdopodobieństwo podane przez użytkownika
+            if (a < p)
+            {
+                double losowaLiczba = rand.NextDouble();
+
+                //symetrycznośc macierzy 1 = istnieje krawędź
+                A[i, j] = 1;
+                A[j, i] = 1;
+                Console.WriteLine("Wierzchołek w: (" + (i + 1) + "," + (j + 1) + ")");
+
+                //Określanie wagi 
+                // w programie podano na czerwono 50 ale 50 daje max wartosc dla 10+50*(1) = 60 a w zalozenaich programu jest W >11 i W <110 wiec mnoznik chyba musi byc 100 albo jestem glupi???
+                int mnoznik = 100;
+                W[i, j] = Math.Floor(10 + mnoznik * (losowaLiczba));
+                W[j, i] = W[i, j];
+
+                //Test czy to dziala
+                //Console.WriteLine(W[i, j] +" - " +W[j, i]);
+            }
         }
     }
-}
 
-//szukanie cykli o dlugosci 3
+    //szukanie cykli o dlugosci 3
 
 
-// TODO WYKMINIC TĄ MACIERZ C W SUMIE + POPRAWIC WAGI
+    // TODO WYKMINIC TĄ MACIERZ C W SUMIE + POPRAWIC WAGI
 
-int licznik_cykli = 0;
+    int licznik_cykli = 0;
+    Cykle C = new Cykle();
 
-for (int i = 1; i < n-1; i++)
-{
-    for (int j = i+1; j < n; j++)
+    for (int i = 0; i < n - 1; i++)
     {
-        if (A[i, j]==1)
+        for (int j = i + 1; j < n; j++)
         {
-            for (int k = 1; k < n; k++)
+            if (A[i, j] == 1)
             {
-                if(A[i,k] == 1 && A[j,k] == 1)
+                for (int k = 1; k < n; k++)
                 {
-                    licznik_cykli++;
-                    double waga = W[i, j]+W[i, k]+W[j, k];
-                    Console.WriteLine("waga:" +waga);
-                    //TO JEST CHYBA ZJEBANE?XD
+                    if (A[i, k] == 1 && A[j, k] == 1 && !C.czyJest(i, j, k))
+                    {
+                        licznik_cykli++;
+                        double waga = W[i, j] + W[i, k] + W[j, k];
+                        int[] temp = new int[] { i, j, k, (int)waga };
+                        C.cykl.Add(temp);
+                        Console.WriteLine("waga:" + waga);
+                        Console.WriteLine($"cykl: {i + 1} {j + 1} {k + 1}");
+                        //TO JEST CHYBA ZJEBANE?XD
+                    }
                 }
             }
         }
     }
+
+
+    //KOD TESTOWY TYLKO DO RYSOWANIA GRAFU NA https://graphonline.ru/en/
+
+    int z = 1;
+    foreach (int e in A)
+    {
+        Console.Write(z % Math.Sqrt(A.Length) == 0 ? $"{e}\n" : e);
+        z += 1;
+    }
+    Console.WriteLine("kontynuowac T/N?");
+    string end = Console.ReadLine();
+    if (end.ToLower()=="n")
+    {
+        break;
+    }
 }
-
-
-// KOD TESTOWY TYLKO DO RYSOWANIA GRAFU NA https://graphonline.ru/en/
-
-//int z = 1;
-//foreach (int e in A)
-//{
-//    Console.Write(z%Math.Sqrt(A.Length)==0 ? $"{e}\n" : e);
-//    z+=1;
-//}   
